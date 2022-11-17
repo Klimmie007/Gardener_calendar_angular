268574c3c97cbab23c39f5a7724b8d4906cd927f
Szymon Gąsiewski <szymongasiewski999@gmail.com>
2022-11-17 19:58:58 +0100

    Moved Form from Preserve component to DefinePreserve component

--- src/app/define-preserve/define-preserve.component.ts
+++ src/app/define-preserve/define-preserve.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./define-preserve.component.css']
 })
 export class DefinePreserveComponent implements OnInit {
+  name: string = '';
+  description: string = '';
+  dateOfProduction: Date = new Date();
+  expirationDate: Date = new Date();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  definePreserve(formData: string[]) {
+    this.name = formData[0];
+    this.description = formData[1];
+    let dateString = '';
+    dateString = formData[4] + '-' + formData[3] + '-' + formData[2];
+    this.dateOfProduction = new Date(dateString);
+    dateString = formData[7] + '-' + formData[6] + '-' + formData[5];
+    this.expirationDate = new Date(dateString);
+  }
 }
--- src/app/preserve/preserve.component.ts
+++ src/app/preserve/preserve.component.ts
@@ -19,14 +19,4 @@ export class PreserveComponent implements OnInit {
     this.dateOfProduction = new Date('2000-03-18');
     this.expirationDate = new Date('2020-03-18');
   }
-
-  definePreserve(formData: string[]) {
-    this.name = formData[0];
-    this.description = formData[1];
-    let dateString = '';
-    dateString = formData[4] + '-' + formData[3] + '-' + formData[2];
-    this.dateOfProduction = new Date(dateString);
-    dateString = formData[7] + '-' + formData[6] + '-' + formData[5];
-    this.expirationDate = new Date(dateString);
-  }
 }

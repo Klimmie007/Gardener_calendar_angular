ef3b7df7170e77f8568239e7ccb800201f2919fa
Szymon Gąsiewski <szymongasiewski999@gmail.com>
2022-11-18 14:04:23 +0100

    Now Preserve component communicates with DefinePreserve component

--- src/app/define-preserve/define-preserve.component.ts
+++ src/app/define-preserve/define-preserve.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, NgForm } from '@angular/forms';
-import { Preserve, preserves } from '../_models/preserve';
+import { Preserve } from '../_models/preserve';
 
 @Component({
   selector: 'app-define-preserve',
@@ -8,10 +8,11 @@ import { Preserve, preserves } from '../_models/preserve';
   styleUrls: ['./define-preserve.component.css']
 })
 export class DefinePreserveComponent implements OnInit {
-  name: string = '';
-  description: string = '';
-  dateOfProduction: Date = new Date();
-  expirationDate: Date = new Date();
+  //name: string = '';
+  //description: string = '';
+  //dateOfProduction: Date = new Date();
+  //expirationDate: Date = new Date();
+  @Input() list: Preserve[] = [];
 
   constructor() {
   }
@@ -20,14 +21,15 @@ export class DefinePreserveComponent implements OnInit {
   }
 
   definePreserve(formData: string[]) {
-    this.name = formData[0];
-    this.description = formData[1];
-    let dateString = '';
-    dateString = formData[4] + '-' + formData[3] + '-' + formData[2];
-    this.dateOfProduction = new Date(dateString);
-    let dateString1 = formData[7] + '-' + formData[6] + '-' + formData[5];
-    this.expirationDate = new Date(dateString1);
-    preserves.push(new Preserve(this.name, this.description, dateString, dateString1));
+    //this.name = formData[0];
+    //this.description = formData[1];
+    let dateOfProduction = formData[4] + '-' + formData[3] + '-' + formData[2];
+    //this.dateOfProduction = new Date(dateString);
+    let expirationDate = formData[7] + '-' + formData[6] + '-' + formData[5];
+    //this.expirationDate = new Date(dateString1);
+    //preserves.push(new Preserve(this.name, this.description, dateString, dateString1));
+    //this.list.push(new Preserve(this.name, this.description, dateString, dateString1));
+    this.list.push(new Preserve(formData[0], formData[1], dateOfProduction, expirationDate));
   }
 
   resetForm(form: NgForm) {
